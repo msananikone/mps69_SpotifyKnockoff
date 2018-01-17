3ed7f7dbfef5c06cb11fe1125cdd8745d77086a3
monichew <monicasananikone@gmail.com>
2018-01-16 20:08:04 -0500

    added DbUtilities class

--- src/mps69_SpotifyKnockoff/Spotify.java
+++ src/mps69_SpotifyKnockoff/Spotify.java
@@ -2,6 +2,8 @@ package mps69_SpotifyKnockoff;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Spotify {
 
@@ -21,6 +23,14 @@ public class Spotify {
 			String sql = "SELECT * FROM users;";
 			
 			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			
+			while(rs.next()) {
+				System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString("last_name"));
+			}
+			
+			//statement.executeUpdate(sql);
+			
 		} catch(InstantiationException | IllegalAccessException | ClassNotFoundException e){
 			e.printStackTrace();
 		}
