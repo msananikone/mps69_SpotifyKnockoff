f50d417dd69ddf9dd738d672089b6ed89a99949d
monichew <monicasananikone@gmail.com>
2018-01-29 02:34:20 -0500

    added album,artist,song classes and tester class

---
+++ src/mps69_SpotifyKnockoff/Album.java
@@ -0,0 +1,23 @@
+/**
+ * class Album
+ * @author Monica Sananikone mps69
+ * @created 1/26/18
+ */
+
+package mps69_SpotifyKnockoff;
+
+import java.util.Map;
+
+public class Album {
+	private String albumID;
+	private String title;
+	private String releaseDate;
+	private String coverImagePath;
+	private String recordingCompany;
+	private int numberOfTracks;
+	private String pmrcRating;
+	private int length;
+	Map <Song, Song> albumSongs;
+	
+
+}
