7fa2eee1fe2a67966690ef5cb68084cad82a0039
monichew <monicasananikone@gmail.com>
2018-01-29 16:22:58 -0500

    created tester

---
+++ src/mps69_SpotifyKnockoff/SpotifyTester.java
@@ -0,0 +1,22 @@
+/**
+ * class SpotifyTester
+ * @author Monica Sananikone mps69
+ * @created 1/26/18
+ */
+
+package mps69_SpotifyKnockoff;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SpotifyTester 
+{
+
+	public static void main(String[] args) {
+		
+	}
+
+}
